7933e3eaf33308a4fd6504d96d681bf4039f9f73
Paolo <paolo9785@gmail.com>
2020-04-29 14:52:53 +0200

    Create regioni.sql

---
+++ regioni.sql
@@ -0,0 +1,30 @@
+CREATE TABLE `regioni` (
+  `ID` int(4) NOT NULL auto_increment,
+  `nome` varchar(50) NOT NULL default '',
+  PRIMARY KEY  (`ID`)
+);
+
+-- 
+-- Dump dei dati per la tabella `regioni`
+-- 
+
+INSERT INTO `regioni` VALUES (1, 'Marche');
+INSERT INTO `regioni` VALUES (2, 'Abruzzo');
+INSERT INTO `regioni` VALUES (3, 'Basilicata');
+INSERT INTO `regioni` VALUES (4, 'Molise');
+INSERT INTO `regioni` VALUES (5, 'Trentino Alto Adige');
+INSERT INTO `regioni` VALUES (6, 'Puglia');
+INSERT INTO `regioni` VALUES (7, 'Calabria');
+INSERT INTO `regioni` VALUES (8, 'Campania');
+INSERT INTO `regioni` VALUES (9, 'Lazio');
+INSERT INTO `regioni` VALUES (10, 'Sardegna');
+INSERT INTO `regioni` VALUES (11, 'Sicilia');
+INSERT INTO `regioni` VALUES (12, 'Toscana');
+INSERT INTO `regioni` VALUES (13, 'Piemonte');
+INSERT INTO `regioni` VALUES (14, 'Emilia Romagna');
+INSERT INTO `regioni` VALUES (15, 'Friuli Venezia Giulia');
+INSERT INTO `regioni` VALUES (16, 'Valle d''Aosta');
+INSERT INTO `regioni` VALUES (17, 'Veneto');
+INSERT INTO `regioni` VALUES (18, 'Liguria');
+INSERT INTO `regioni` VALUES (19, 'Lombardia');
+INSERT INTO `regioni` VALUES (20, 'Umbria');
